e1762aa9bd45b41f7e505f21f2dfbe777565add6
Vincent Batts <vbatts@hashbangbash.com>
2016-10-24 14:40:10 -0400

    unit: more defaults and easier ExecStart
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- unit/template.go
+++ unit/template.go
@@ -1,7 +1,9 @@
 package unit
 
 import (
+	"fmt"
 	"html/template"
+	"strings"
 
 	"github.com/coreos/go-systemd/unit"
 )
@@ -9,16 +11,31 @@ import (
 // DefaultOptions includes the boiler plate options for generating OCI layout
 // unit files.
 var DefaultOptions = []*unit.UnitOption{
-	&unit.UnitOption{
-		Section: "Unit",
-		Name:    "Description",
-		Value:   "OCI: %n",
-	},
-	&unit.UnitOption{
-		Section: "Service",
-		Name:    "Slice",
-		Value:   "oci.slice",
-	},
+	&unit.UnitOption{Section: "Unit", Name: "Description", Value: "OCI: %n"},
+	&unit.UnitOption{Section: "Service", Name: "Slice", Value: "oci.slice"},
+	&unit.UnitOption{Section: "Service", Name: "PrivateTmp", Value: "yes"},
+	&unit.UnitOption{Section: "Service", Name: "ProtectSystem", Value: "yes"},
+	&unit.UnitOption{Section: "Service", Name: "ProtectHome", Value: "yes"},
+	&unit.UnitOption{Section: "Service", Name: "Delegate", Value: "yes"},
+	&unit.UnitOption{Section: "Service", Name: "DevicePolicy", Value: "closed"},
 }
 
-var shellExecTemplate = template.Must(template.New("shellExec").Parse(`/bin/sh -c "{{.}}"`))
+// ExecStart provides the unit file option for ExecStart=, given a command string
+func ExecStart(cmd string) (*unit.UnitOption, error) {
+	// if the command is not an abosulte path
+	if !strings.HasPrefix(cmd, "/") {
+		cmd = fmt.Sprintf(`/bin/sh -c %q`, cmd)
+	}
+	return unit.NewUnitOption("Service", "ExecStart", cmd), nil
+}
+
+var (
+	shellExecTemplate = template.Must(template.New("shellExec").Parse(`/bin/sh -c "{{.}}"`))
+)
+
+/*
+Needed:
+from systmed.exec(5)
+- RootDirectory=
+
+*/
--- unit/template_test.go
+++ unit/template_test.go
@@ -17,3 +17,37 @@ func TestShellExec(t *testing.T) {
 		t.Errorf("expected %q; got %q", expect, got)
 	}
 }
+
+func TestExecStart(t *testing.T) {
+	expect := `/bin/sh -c "tail -f /dev/null"`
+	u, err := ExecStart(`tail -f /dev/null`)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if u.Section != "Service" {
+		t.Errorf("Expected unit option in Service; got %q", u.Section)
+	}
+	if u.Name != "ExecStart" {
+		t.Errorf("Expected unit option of ExecStart; got %q", u.Name)
+	}
+	if u.Value != expect {
+		t.Errorf("Expected unit value of %q; got %q", expect, u.Value)
+	}
+
+	expect = `/usr/bin/tail -f /dev/null`
+	u, err = ExecStart(expect)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if u.Section != "Service" {
+		t.Errorf("Expected unit option in Service; got %q", u.Section)
+	}
+	if u.Name != "ExecStart" {
+		t.Errorf("Expected unit option of ExecStart; got %q", u.Name)
+	}
+	if u.Value != expect {
+		t.Errorf("Expected unit value of %q; got %q", expect, u.Value)
+	}
+}
